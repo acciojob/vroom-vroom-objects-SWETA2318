b212e4dcbf1b27591d96df449e0e840f14ee1cfd
SWETA2318 <150820032+SWETA2318@users.noreply.github.com>
2024-01-11 18:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-// Complete the js code
-function Car(make, model) {}
+// Car constructor
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Adding a method to Car prototype
+Car.prototype.getMakeModel = function () {
+    return this.make + ' ' + this.model;
+};
+
+// SportsCar constructor inheriting from Car
+function SportsCar(make, model, topSpeed) {
+    // Call the Car constructor to initialize make and model
+    Car.call(this, make, model);
+    
+    this.topSpeed = topSpeed;
+}
+
+// Inherit the Car prototype in SportsCar
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Adding a method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function () {
+    return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
